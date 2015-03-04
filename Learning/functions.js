d37b9590e532461ae05aa239744bf320a542eedc
sachinio <sachin.patney@gmail.com>
2015-03-03 23:56:26 -0800

    updating presentation

--- Learning/functions.js
+++ Learning/functions.js
@@ -66,12 +66,13 @@ var section = d3.selectAll("section"),
 
 createCircles();
 createAxis();
-//createDots();
+createZoom();
 createQuadTreeSearch();
 enter();
 update();
 exit();
 setUpClock();
+expandSitePage();
 
 aniCir.on('click', function () {
         removeAnimatedCircles();
@@ -121,6 +122,12 @@ function deactivate(d, i) {
     }else if( i=== scatterIndex){
         removeDots();
     }
+}
+
+function expandSitePage(){
+    var site = $('#d3site');
+
+    site.find('iframe').height(site.height()).width(site.width());
 }
 
 function stringFill(x, n) {
@@ -951,4 +958,63 @@ function setUpClock(){
     }, 1000);
 
     d3.select(self.frameElement).style("height", height + "px");
+}
+
+function createZoom(){
+    var zoomDiv = $('#zoomSvg');
+
+    var margin = {top: 20, right: 20, bottom: 30, left: 40},
+        width = zoomDiv.width() - margin.left - margin.right,
+        height = zoomDiv.height() - margin.top - margin.bottom;
+
+    var x = d3.scale.linear()
+        .domain([-width / 2, width / 2])
+        .range([0, width]);
+
+    var y = d3.scale.linear()
+        .domain([-height / 2, height / 2])
+        .range([height, 0]);
+
+    var xAxis = d3.svg.axis()
+        .scale(x)
+        .orient("bottom")
+        .tickSize(-height);
+
+    var yAxis = d3.svg.axis()
+        .scale(y)
+        .orient("left")
+        .ticks(5)
+        .tickSize(-width);
+
+    var zoom = d3.behavior.zoom()
+        .x(x)
+        .y(y)
+        .scaleExtent([1, 32])
+        .on("zoom", zoomed);
+
+    var svg = d3.select(zoomDiv.get(0)).append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+        .call(zoom);
+
+    svg.append("rect")
+        .attr("width", width)
+        .attr("height", height);
+
+    svg.append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + height + ")")
+        .call(xAxis);
+
+    svg.append("g")
+        .attr("class", "y axis")
+        .call(yAxis);
+
+    function zoomed() {
+        svg.select(".x.axis").call(xAxis);
+        svg.select(".y.axis").call(yAxis);
+    }
+
 }
